3e77ad6b6672167b316fd417a3b1580ea788ff7f
TataneCode <sylvain.turpault@pm.me>
2026-01-26 21:34:26 +0100

    Continue understanding rust

--- lesson-06/storage_system/src/services/processor.rs
+++ lesson-06/storage_system/src/services/processor.rs
@@ -1,7 +1,9 @@
+use std::vec;
+
 use crate::{models::storage::Storage};
 
 pub trait IProcessor {
-    fn proceed(&mut self) -> bool;
+    fn proceed(&mut self) -> Result<(), String>;
 }
 
 pub struct Processor<T: Storage> {
@@ -13,45 +15,51 @@ impl<T: Storage> Processor<T> {
         storage,
     } }
 
-    fn add_key(storage: &mut T, key: &str, value: &str) {
+    fn add_key(storage: &mut T, key: &str, value: &str) -> Result<String, String> {
         println!("Ajout valeur {} - {}", key, value);
         match storage.save(key, value) {
-            Err(error) => println!("{}", error),
-            Ok(_) => println!("Réussi"),
+            Err(error) => Err(error),
+            Ok(_) => Ok("Réussi".to_string()),
         }
     }
 
-    fn show_value(memory: &T, key: &str) {
+    fn show_value(memory: &T, key: &str) -> Result <String, String> {
         println!("Affichage de la valeur pour la clef '{}'", key);
         match memory.load(key) {
-            Err(error) => println!("{}", error),
-            Ok(value) => println!("==> '{}'", value),
+            Err(error) => Err(error),
+            Ok(value) => Ok(format!("==> '{}'", value)),
         }
     }
 
-    fn delete_value(memory: &mut T, key: &str) {
+    fn delete_value(memory: &mut T, key: &str) -> Result<String, String> {
         println!("Suppression de la valeur pour la clef '{}'", key);
         match memory.delete(key) {
-            Err(error) => println!("{}", error),
-            Ok(_) => println!("Réussi"),
+            Err(error) => Err(error),
+            Ok(_) => Ok("Réussi".to_string()),
         }
     }
 }
 
 impl<T: Storage> IProcessor for Processor<T> {
-    fn proceed(&mut self) -> bool {
-        Self::add_key(&mut self.storage, "1", "1");
-        Self::add_key(&mut self.storage, "2", "2");
-        Self::add_key(&mut self.storage, "1", "2");
+    fn proceed(&mut self) -> Result<(), String> {
+        let mut results: Vec<Result<String, String>> = Vec::new();
 
-        Self::show_value(&self.storage, "1");
-        Self::show_value(&self.storage, "2");
-        Self::show_value(&self.storage, "3");
+        results.push(Self::add_key(&mut self.storage, "1", "1"));
+        results.push(Self::add_key(&mut self.storage, "2", "2"));
+        results.push(Self::add_key(&mut self.storage, "1", "2"));
 
-        Self::delete_value(&mut  self.storage, "1");
-        Self::delete_value(&mut self.storage, "3");
+        results.push(Self::show_value(&self.storage, "1"));
+        results.push(Self::show_value(&self.storage, "2"));
+        results.push(Self::show_value(&self.storage, "3"));
 
-        return true;
+        results.push(Self::delete_value(&mut  self.storage, "1"));
+        results.push(Self::delete_value(&mut self.storage, "3"));
+
+        if results.into_iter().all(|res| res.is_ok()) {
+            Ok(())
+        } else {
+            Err("Tout ne s'est pas bien déroulé !".to_string())
+        }
     }
 }
 
@@ -64,6 +72,7 @@ mod tests {
         let storage = MockStorage::new(true);
         let mut processor = Processor::new(storage);
         let result = processor.proceed();
-        assert!(result);
+        
+        assert!(result.is_ok());
     }
 }
